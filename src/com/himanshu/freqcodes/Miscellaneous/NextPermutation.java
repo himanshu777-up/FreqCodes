f410640b477b79fabf4359123de714458673cb3e
Himanshu Upadhyay <himanshu.14.upadhyay@gmail.com>
2022-01-13 22:17:13 +0530

    Next lexico graphical permuation of array

--- src/com/himanshu/freqcodes/Miscellaneous/NextPermutation.java
+++ src/com/himanshu/freqcodes/Miscellaneous/NextPermutation.java
@@ -21,30 +21,30 @@ public class NextPermutation {
     private static void nextPerm(int[] arr) {
 
         int len = arr.length;
-        int i = len-2;
+        int i = len - 2;
 
         //Finding where the graph starts descending
-        while(i>=0 && arr[i]>=arr[i+1]){
+        while (i >= 0 && arr[i] >= arr[i + 1]) {
             i--;
         }
 
         //If no permutation is possible, i reaches to -1
-        if(i==-1){
+        if (i == -1) {
             System.out.println("No next permutation possible here");
-            return ;
+            return;
         }
 
         //Find the smallest number greater than the arr[i]
-        int k = len-1;
-        while(arr[k]<=arr[i])   k--;
+        int k = len - 1;
+        while (arr[k] <= arr[i]) k--;
 
         //Swap these two elements
         swap(arr, i, k);
 
         //Now sort the remaining of the array, but since it is already sorted in descending order, we need to just reverse it
-        int l =  len-1;
-        while(l>i){
-            swap(arr, i+1, l);  //starting with (i+1) as we don't want to swap the ith element
+        int l = len - 1;
+        while (l > i) {
+            swap(arr, i + 1, l);  //starting with (i+1) as we don't want to swap the ith element
             l--;
             i++;
         }
